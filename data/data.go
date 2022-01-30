c11e317d5abbd6939d54130259b358822461a945
Charles Seat <crseat@gmail.com>
2022-01-29 17:34:19 -0800

    Add create table functionality and refactor errors

--- data/data.go
+++ data/data.go
@@ -2,6 +2,8 @@ package data
 
 import (
 	"database/sql"
+	"github.com/crseat/patient_feedback/errs"
+	"github.com/crseat/patient_feedback/logger"
 	_ "github.com/mattn/go-sqlite3"
 )
 
@@ -10,9 +12,33 @@ var db *sql.DB
 func OpenDb() error {
 	var err error
 
-	db, err = sql.Open("sqlite3", "./sqlite-db.db")
+	db, err = sql.Open("sqlite3", "./patientResponseDb.db")
 	if err != nil {
 		return err
 	}
 	return db.Ping()
+}
+
+// CreateTable creates the responses table in the sqlLite db so we can record responses.
+func CreateTable() {
+	createTbl := `CREATE TABLE IF NOT EXISTS responses (
+        "recommendNumber" INTEGER,
+        "explainedWell" TEXT,
+        "diagnosisFeeling" TEXT
+      );`
+
+	// Prepare our statement
+	stmt, err := db.Prepare(createTbl)
+	if err != nil {
+		appError := errs.NewAppError("Error while preparing create statement for responses table")
+		logger.ErrorLogger.Println(appError)
+	}
+
+	// Execute our statement
+	_, err = stmt.Exec()
+	if err != nil {
+		appError := errs.NewAppError("Error while executing create statement for responses table")
+		logger.ErrorLogger.Println(appError)
+	}
+	logger.InfoLogger.Println("Responses table created")
 }
--- errs/errors.go
+++ errs/errors.go
@@ -1,32 +1,14 @@
 //Package errs defines and organizes error types and functions
 package errs
 
-import "net/http"
-
 type AppError struct {
 	Code    int
 	Message string
 }
 
-// AsMessage retrieves the message string from current AppError
-func (e AppError) AsMessage() *AppError {
-	return &AppError{
-		Message: e.Message,
-	}
-}
-
-// NewValidationError defines the parameters for an AppError that occurs when a request is invalid.
-func NewValidationError(message string) *AppError {
+// NewAppError defines the parameters for an AppError that occurs when a request is invalid.
+func NewAppError(message string) *AppError {
 	return &AppError{
 		Message: message,
-		Code:    http.StatusUnprocessableEntity,
-	}
-}
-
-// NewUnexpectedError defines the parameters for an AppError that occurs when an unexpected error occurs.
-func NewUnexpectedError(message string) *AppError {
-	return &AppError{
-		Message: message,
-		Code:    http.StatusInternalServerError,
 	}
 }
